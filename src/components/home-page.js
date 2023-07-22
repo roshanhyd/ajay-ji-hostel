25f1eac11911f20ac70781840f796101727d5cc7
Roshan Singh <roshan.in.pune@gmail.com>
2023-07-23 00:40:21 +0530

    Added Home Page

---
+++ src/components/home-page.js
@@ -0,0 +1,131 @@
+/* eslint-disable react/style-prop-object */
+export default function HomePage() {
+  return (
+    <>
+      <p>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          001
+        </span>
+      </p>
+      <p>
+        <span
+          style={{
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          101
+        </span>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          102
+        </span>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          103
+        </span>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          104
+        </span>
+      </p>
+      <p>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          201
+        </span>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          202
+        </span>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          203
+        </span>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          204
+        </span>
+      </p>
+      <p>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          301
+        </span>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          302
+        </span>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          303
+        </span>
+        <span
+          style={{
+            
+            borderStyle: "solid",
+            borderColor: "#FF0000"
+          }}
+        >
+          304
+        </span>
+      </p>
+    </>
+  );
+}
